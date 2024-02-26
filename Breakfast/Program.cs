a05a41db40595d45d96febf7f66011ace4289654
X6STRIVE <X6STRIVE@IN05N001NS>
2024-02-26 14:22:06 +0530

    Add project files.

---
+++ Breakfast/Program.cs
@@ -0,0 +1,105 @@
+﻿
+using System.Diagnostics;
+
+namespace AsyncBreakfast
+{
+    // These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
+    internal class Bacon { }
+    internal class Coffee { }
+    internal class Egg { }
+    internal class Juice { }
+    internal class Toast { }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            Task<Egg> eggsTask = FryEggs(2);
+            Task<Bacon> baconTask = FryBacon(3);
+            Task<Toast> toastTask = ToastBread(2).ContinueWith(t => {
+                Toast toast = t.Result;
+                ApplyButter(toast);
+                ApplyJam(toast);
+                Console.WriteLine("toast is ready");
+                return toast;
+            });
+
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            await Task.WhenAll(eggsTask, baconTask, toastTask);
+            
+            Console.WriteLine("eggs are ready");
+
+            Console.WriteLine("bacon is ready");
+
+            Console.WriteLine("Breakfast is ready!");
+            stopWatch.Stop();
+            Console.Write($"Time Elapsed:{stopWatch.Elapsed}");
+        }
+
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        private async static Task<Toast> ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            await Task.Delay(3000);
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        private async static Task<Bacon> FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            await Task.Delay(3000);
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private async static Task<Egg> FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            await Task.Delay(3000);
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+}
